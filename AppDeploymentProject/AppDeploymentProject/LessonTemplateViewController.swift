516e244cb010a41d7406a039acbee947f2560e7b
Levi Hand <lhand1986@yahoo.com>
2016-06-08 15:21:42 -0700

    Initial Commit
    Fleshed out the basic set up for the view controllers, added a swift
    file for a globally available user class, and ensured that all of the
    navigation functionality was available.

---
+++ AppDeploymentProject/AppDeploymentProject/LessonTemplateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LessonTemplateViewController.swift
+//  AppDeploymentProject
+//
+//  Created by Levi Hand on 6/8/16.
+//  Copyright © 2016 Levi Hand. All rights reserved.
+//
+
+import UIKit
+
+class LessonTemplateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppDeploymentProject/AppDeploymentProject/UserSingleton.swift
@@ -0,0 +1,14 @@
+//
+//  UserSingleton.swift
+//  AppDeploymentProject
+//
+//  Created by Levi Hand on 6/8/16.
+//  Copyright © 2016 Levi Hand. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserSingleton {
+    
+}
