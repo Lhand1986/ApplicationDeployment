f3f570f3e2600d5099c9a6f8ab02401bec55c747
Levi Hand <lhand1986@yahoo.com>
2016-06-19 18:16:11 -0700

    Custom Calendar Cell file
    Added functionality to the calendar cell file, importing the UIKit to
    allow creation of a label. This function will likely be moved in the
    near future.
    
    Currently working on solutions to add reactive layout design to the
    label inside the collection view cell. This is so the user can add
    appointment information to the cells. This will probably be removed and
    placed in the main View Controller file and accessed inside one of the
    Collection View Delegate functions.

---
+++ AppDeploymentProject/AppDeploymentProject/CustomCalendarCell.swift
@@ -0,0 +1,43 @@
+//
+//  CustomCalendarCell.swift
+//  AppDeploymentProject
+//
+//  Created by Levi Hand on 6/16/16.
+//  Copyright © 2016 Levi Hand. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var dayView: UIView!
+    
+    var labelPosition: CGFloat!
+    
+    //Function that allows creation of a label inside the custom calendar cell when called
+    func createLabel(appt: String){
+        let dayHeight = CGFloat(dayView.bounds.size.height)
+        let dayWidth = CGFloat(self.bounds.size.width)
+        let label = UILabel()
+//        let leading = NSLayoutConstraint(item: label, attribute: .LeadingMargin, relatedBy: .Equal, toItem: UIView(), attribute: .LeadingMargin, multiplier: 1.0, constant: 20)
+        labelPosition = dayHeight
+        label.frame = CGRectMake(6, (labelPosition), dayWidth, 30)
+        label.backgroundColor = .brownColor()
+        label.textColor = .blackColor()
+        label.font = label.font.fontWithSize(10)
+        label.textAlignment = .Left
+        label.text = appt
+        label.minimumScaleFactor = 0.1
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.addConstraint(leading)
+        label.centerXAnchor
+        label.centerYAnchor
+        
+        
+        
+        dayView.addSubview(label)
+    }
+    
+    
+}
