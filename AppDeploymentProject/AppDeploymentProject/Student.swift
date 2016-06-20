cbbd012f7da066537472c15cee211d74eac298e5
Levi Hand <lhand1986@yahoo.com>
2016-06-19 18:16:42 -0700

    Added a student swift file
    Added a student class to handle the student information that will be
    pushed to core data.

---
+++ AppDeploymentProject/AppDeploymentProject/Student.swift
@@ -0,0 +1,16 @@
+//
+//  Student.swift
+//  AppDeploymentProject
+//
+//  Created by Levi Hand on 6/19/16.
+//  Copyright © 2016 Levi Hand. All rights reserved.
+//
+
+import Foundation
+
+class Student {
+    var name: String!
+    var lessonsRemain: String!
+    var lessonsComplete: String!
+    var scheduleArray: [NSDate]!
+}
